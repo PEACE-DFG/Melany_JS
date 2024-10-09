04281c9f469608f12d1e5b2da02feaaa4964cc0a
PEACE-DFG <awofesobipeace@gmail.com>
2024-10-09 12:26:11 +0100

    introduction to js

---
+++ script.js
@@ -0,0 +1,24 @@
+// window.alert('Welcome Home')
+// document.write("I am a girl")
+document.getElementById('root').innerHTML="Story Time";
+document.getElementById('text').innerHTML=`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non labore optio, ex aliquid sed, et iusto natus nam quas quae consequuntur officia qui repellendus rem, voluptas ut alias. Iste, nam!
+    Ipsum esse eveniet autem eaque laudantium veniam ut, iure officia pariatur, dolores nisi accusantium. Repudiandae nesciunt esse dignissimos sunt animi, magni dolore facilis! Velit fugit qui laboriosam dignissimos ratione adipisci!
+    Praesentium blanditiis expedita totam nobis sint repellat magnam possimus saepe repellendus sapiente dolores qui velit nulla cupiditate assumenda vero, eius asperiores accusantium quam alias placeat molestiae sunt! Perferendis, voluptate excepturi.
+    Quidem reprehenderit deserunt veniam alias consectetur, maiores ab beatae, totam sapiente laborum saepe natus corrupti cum quia. Quasi consequuntur praesentium dolorem voluptates dolores dolorum. Accusamus tenetur nulla magnam impedit dolores?
+    Repellendus eaque tempora quas alias, laborum dicta ipsam ad, numquam sapiente perspiciatis, fugit eligendi sequi quidem deleniti quos libero eum delectus molestias. Laborum dolorem quibusdam minus aut labore, adipisci velit!
+    Atque veritatis repellat, asperiores ab, enim voluptatibus dolores facilis hic recusandae saepe, eius sint earum aut accusamus! Voluptatibus, minima aspernatur nostrum distinctio aut cum explicabo iure dolore nam? Porro, doloribus.
+    Eveniet eius aperiam rerum ducimus dolores esse error blanditiis magni debitis, aspernatur eum quo, praesentium similique, sequi eaque hic provident veniam illo corrupti commodi velit in voluptatem. Doloremque, dignissimos tenetur!
+    Ea vitae impedit rerum voluptates illum laboriosam eaque, placeat inventore maxime nihil, alias quod expedita perspiciatis temporibus accusamus sapiente adipisci unde? Sint doloremque aspernatur consectetur alias ex sit, quidem error?
+    Corporis temporibus itaque corrupti laudantium, excepturi voluptatum inventore est? Vitae quam quod accusantium, placeat laborum possimus reprehenderit pariatur minus eum ad eius saepe, debitis mollitia voluptatibus voluptates exercitationem quo sint?
+    Sit cumque eveniet enim quasi repudiandae, mollitia labore quod dolorum possimus expedita quaerat nam ut dolorem? Tempora, placeat. Maiores magni aliquid obcaecati iusto dolorum voluptatem facere, voluptates omnis aperiam possimus!`
+
+
+    // JS SYNTAX
+
+    let x = 5;
+    let y = 6;
+
+    let z = x + y;
+
+    console.log(z)
+    document.write(z)
